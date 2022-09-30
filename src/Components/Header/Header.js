aacaa319f5750f514930c286c30349640f98a4d8
mehnazimu <mehnaz.imu@gmail.com>
2022-09-30 12:13:22 +0600

    font-awesome added

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,11 +1,14 @@
+import { faDumbbell } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import './Header.css'
 
 const Header = () => {
     return (
         <div>
-            <nav>
-                <h2>Fit-Club</h2>
+            <nav className='header'>
+                <FontAwesomeIcon className='font' icon={faDumbbell}></FontAwesomeIcon>
+                <h2 className='header-text'>Fit-Club</h2>
             </nav>
         </div>
     );
